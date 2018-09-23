6e76ea99566a293e47209c23ed5f7d08adf92b02
void <thevoidf@gmail.com>
2018-09-23 12:42:32 +0400

    updated examples

--- examples/example.py
+++ examples/example.py
@@ -4,19 +4,18 @@ sys.path.append(dirname(dirname(realpath(__file__))))
 from tb import Table
 
 t = Table()
-t.set_header(['First', 'Last'])
-t.add_row(['julian', 'casablancas'])
-t.add_row(['someone', 'else'])
+t.header = ['First', 'Last']
+t.rows.append(['Julian', 'Jasablancas'])
+t.rows.append(['Someone', 'Else'])
 t.render()
 
 t = Table()
-t.set_header(['first', 'last'])
-t.rows.append(['ethan', 'kath'])
-t.rows.append(['alice', 'glass'])
+t.header = ['First', 'Last']
+t.rows.append(['Ethan', 'Kath'])
 print(t)
 
 t = Table(
-	['first', 'last', 'more'],
+	['First', 'Last', 'More'],
 	[
 		['first', 'second', 'more'],
 		['third', 'fourth', 'more'],
